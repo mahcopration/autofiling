ac7cf8b722c0299fd1f32f4e93a03361acd42c61
UnknownBotz <145199654+UnknownBotz@users.noreply.github.com>
2023-09-29 17:21:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rohaniscoder/EasyAdvAutoFilter.git /EasyAdvAutoFilter
+  git clone https://github.com/UnknownBotz/EasyAdvAutoFilter.git /EasyAdvAutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EasyAdvAutoFilter
