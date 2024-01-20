8411b5429cf00d01b939c9d05141c9fe70e186f0
minemahsoom <149931233+minemahsoom@users.noreply.github.com>
2024-01-21 02:40:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -671,7 +671,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/UNKNOWN_MAN_ON_MISSION")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Mahsoommjm")
                                 ]
                             ]
                         )
