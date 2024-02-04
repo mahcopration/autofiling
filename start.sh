3bce361d5a9e442c00008e9c5bd8d5ef232310ae
mahcopration <129929195+mahcopration@users.noreply.github.com>
2024-02-04 16:30:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /autofiling
 fi
-cd /autofiling
+cd /autofilingsou
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
